b3861497233e6c621b442b5fe4b2a6e707d43e2b
Senne0165 <99189622+Senne0165@users.noreply.github.com>
2022-02-15 11:52:21 +0100

    Add files via upload

---
+++ Ziekmelden/Ziekmelden/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ziekmelden
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgZieken.ItemsSource = db.Zieks.ToList();
+        }
+
+        private void btnVerstuur_Click(object sender, RoutedEventArgs e)
+        {
+            Persoon per = new Persoon();
+            per.Voornaam = txtVoornaam.Text;
+            per.Achternaam = txtAchternaam.Text;
+            per.Afdeling = txtAfdeling.Text;
+
+            Ziek dat = new Ziek();
+            dat.DatumZ = dateZiek.DisplayDate;
+            dat.DatumB = dateBeter.DisplayDate;
+            dat.Persoon= per;
+
+            
+            db.Zieks.InsertOnSubmit(dat);
+            db.SubmitChanges();
+        }
+
+        private void dgZieken_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
+
